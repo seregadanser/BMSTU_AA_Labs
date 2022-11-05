1aed8cab1c30ef93761cd6b66bdc15b05a6d4365
Sergey <kalashnikov.sergey01@gmail.com>
2022-11-05 22:37:48 +0300

    up

--- Лаб2/AA2/TimeCompare.cpp
+++ Лаб2/AA2/TimeCompare.cpp
@@ -9,72 +9,9 @@ void time_compare(gen_func_t gen_func) {
     double start;
     double end;
     double copy_t;
-    int repeats = 2000;
+    int repeats = 1000;
     cout << setw(33) << "Time in ms" << endl;
     cout << setw(3) << "Size" << setw(15) << "Easy" << setw(20) << "Vinograde" << setw(20) << "VinogradeOptimise" << endl;
-    for (int len = 10; len < 100; len += 10) {
-        cout << setw(3) << len;
-        data_t m1, m2;
-        gen_func(&m1, len);
-        gen_func(&m2, len);
-
-       
-        start = getCPUTime();
-        for (int i = 0; i < repeats; i++) {
-            matrix_multiplication(m1, m2);
-        }
-        end = getCPUTime();
-        cout << setw(15) << (end - start) / repeats;
-
-
-        start = getCPUTime();
-        for (int i = 0; i < repeats; i++) {
-            matrix_multiplication_Vinograd(m1, m2);
-        }
-        end = getCPUTime();
-        cout << setw(15) << (end - start) / repeats ;
-
-        start = getCPUTime();
-        for (int i = 0; i < repeats; i++) {
-            matrix_multiplication_VinogradOptimase(m1, m2);
-        }
-        end = getCPUTime();
-        cout << setw(15) << (end - start) / repeats << endl;
-        free_matrix(m1.matrix, len);
-        free_matrix(m2.matrix, len);
-    }
-
-    for (int len = 100; len < 600; len += 100) {
-        cout << setw(3) << len;
-        data_t m1, m2;
-        gen_func(&m1, len);
-        gen_func(&m2, len);
-
-
-        start = getCPUTime();
-        for (int i = 0; i < repeats; i++) {
-            matrix_multiplication(m1, m2);
-        }
-        end = getCPUTime();
-        cout << setw(15) << (end - start) / repeats ;
-
-
-        start = getCPUTime();
-        for (int i = 0; i < repeats; i++) {
-            matrix_multiplication_Vinograd(m1, m2);
-        }
-        end = getCPUTime();
-        cout << setw(15) << (end - start) / repeats ;
-
-        start = getCPUTime();
-        for (int i = 0; i < repeats; i++) {
-            matrix_multiplication_VinogradOptimase(m1, m2);
-        }
-        end = getCPUTime();
-        cout << setw(15) << (end - start) / repeats<< endl;
-        free_matrix(m1.matrix, len);
-        free_matrix(m2.matrix, len);
-    }
     for (int len = 11; len < 101; len += 10) {
         cout << setw(3) << len;
         data_t m1, m2;
