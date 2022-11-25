da46f5e53e87d6ac04f11e1550f1265229721ecd
Sergey <kalashnikov.sergey01@gmail.com>
2022-11-26 01:19:58 +0300

    up

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+
+
+
+
+delegate char Action(char a, int key);
+
+class Conver
+{
+    Action a;
+    Conver nextState;
+    Queue<char> queue;
+    int key;
+    public void Start()
+    {
+
+    }
+
+    public void Process()
+    {
+        char elem = '\0';
+        bool e = false;
+        if (queue.Count > 0)
+            e = queue.TryDequeue(out elem);
+
+        if(e)
+        {
+            a.Invoke(elem, key);
+            nextState.Add(elem);
+        }
+
+    }
+
+    public void Add(char c)
+    {
+        queue.Enqueue(c);
+    }
+}
