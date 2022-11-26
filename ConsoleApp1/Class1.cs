ff4321c1b62f2d778c493bae18017cedcce06e7f
Sergey <kalashnikov.sergey01@gmail.com>
2022-11-26 22:32:52 +0300

    up

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate char Action(char a, int key);
+    static class  Crypto
+    {
+        const string alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+      static public char CodeEncode(char text, int k)
+        {
+            var fullAlfabet = alfabet + alfabet.ToLower();
+            var letterQty = fullAlfabet.Length;
+            char retVal = '\0';
+
+            var index = fullAlfabet.IndexOf(text);
+            if (index < 0)
+            {
+                retVal = text;
+            }
+            else
+            {
+                var codeIndex = (letterQty + index + k) % letterQty;
+                retVal = fullAlfabet[codeIndex];
+            }
+
+            return retVal;
+        }
+
+       static char GetRandomKey(int k)
+        {
+            char gamma = '\0';
+            var rnd = new Random(k);
+            gamma = (char)rnd.Next(97, 123);
+            return gamma;
+        }
+
+       static public char Cipher(char text, int key)
+        {
+            var currentKey = GetRandomKey(key);
+            char res = '\0';
+            res = ((char)(text ^ key));
+            return res;
+        }
+
+       
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,114 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+using System.Runtime.CompilerServices;
+
+string input = Console.ReadLine();
+Queue<Ask> q = new Queue<Ask>();
+
+foreach (char c in input)
+    q.Enqueue(new Ask(4, c));
+q.Enqueue(new Ask(3, '\0', true));
+Console.WriteLine(q.Count);
+Queue<Ask> exit = new Queue<Ask>();
+Paral();
+Console.WriteLine(exit.Count);
+
+void Paral()
+{
+    Thread[] t = new Thread[3];
+    Conver[] c = new Conver[3];
+  
+    c[0] = new Conver(new ConsoleApp1.Action(Crypto.CodeEncode),3, q, 0);
+    c[1] = new Conver(new ConsoleApp1.Action(Crypto.Cipher), 4, 1);
+    c[2] = new Conver(new ConsoleApp1.Action(Crypto.CodeEncode), 5, 2);
+
+    c[0].nextState = c[1].queue;
+    c[1].nextState = c[2].queue;
+    c[2].nextState = exit;
+
+    for (int i = 0; i < 3; i++)
+    {
+        t[i] = new Thread(c[i].Start);
+        t[i].Start();
+    }
+
+    foreach (Thread thread in t)
+    {
+        thread.Join();
+    }
+}
 
 
+class Ask
+{
+    public long[] in_time, out_time;
+    public char elem;
+    public bool last;
+    public Ask(int n, char elem, bool last = false)
+    {
+        in_time = new long[n];
+        out_time = new long[n];
+        this.elem = elem;
+        this.last = last;
+    }
+}
 
-
-delegate char Action(char a, int key);
-
+enum state { finish, empty, ok };
 class Conver
 {
-    Action a;
-    Conver nextState;
-    Queue<char> queue;
+    ConsoleApp1.Action a;
+    int id;
+    public Queue<Ask> nextState;
+    public Queue<Ask> queue;
     int key;
+
+    public Conver(ConsoleApp1.Action a, int key, int id)
+    {
+        this.a = a;
+        queue = new Queue<Ask>();
+        this.key = key;
+        this.id = id;
+    }
+    public Conver(ConsoleApp1.Action a, int key, Queue<Ask> q, int id) : this(a, key, id)
+    {
+        queue = q;
+    }
+
     public void Start()
     {
-
-    }
-
-    public void Process()
-    {
-        char elem = '\0';
-        bool e = false;
-        if (queue.Count > 0)
-            e = queue.TryDequeue(out elem);
-
-        if(e)
+        state s = state.ok;
+        while(s!=state.finish)
         {
-            a.Invoke(elem, key);
-            nextState.Add(elem);
+            s = Process();
         }
-
     }
 
-    public void Add(char c)
+    public state Process()
     {
-        queue.Enqueue(c);
+         Ask elem = null;
+        bool e = false;
+        lock (queue)
+        {
+            if (queue.Count > 0)
+                e = queue.TryDequeue(out elem);
+        }
+        if (e && elem.last)
+        {
+            elem.in_time[id + 1] = DateTime.Now.Ticks;
+            lock (nextState)
+            { nextState.Enqueue(elem); }
+            return state.finish; }
+        if (e)
+        {
+            elem.elem = a.Invoke(elem.elem, key);
+            lock (nextState)
+            {
+                elem.in_time[id + 1] = DateTime.Now.Ticks;
+                nextState.Enqueue(elem);
+            }
+        }
+       
+        return state.ok;
     }
+
+  
 }
