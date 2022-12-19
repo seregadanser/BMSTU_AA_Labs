d58ce9f92a3ef6c25dd1c409dae6834765a49f64
Sergey <kalashnikov.sergey01@gmail.com>
2022-12-19 21:47:14 +0300

    up

--- Лаб5/ConsoleApp1/Class2.cs
+++ Лаб5/ConsoleApp1/Class2.cs
@@ -11,31 +11,36 @@ namespace ConsoleApp1
     {
         public static void FormatOut(Queue<Ask> list)
         {
+            ss = 0;
+            ww = 0;
             //while (list.Count > 0)
             foreach (Ask a in list)
                 FElem(a);
             //FElem(list.Dequeue());
 
-            for(int i = 0; i < list.Peek().work_time.Length - 1; i++)
-            {
-
-            }
-            Console.WriteLine($"Total wait time in queue {ss}");
+           
+            Console.WriteLine($"Total wait time in queue {ss}, work time {ww}");
             Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         }
-        static long ss = 0; 
+        static long ss = 0;
+        static long ww = 0;
         public static void FElem(Ask element)
         {
             long summ = 0;
+            long w = 0;
             for(int i = 0; i<element.work_time.Length-1;i++)
             {
-                summ += (element.out_time[i] - element.in_time[i]) / 10000;
-                Console.WriteLine($"Conv {i}: {element.state[i]} -> {element.state[i+1]}" + "|" + $"in q {element.in_time[i]}, out q {element.out_time[i]}, wait {(element.out_time[i] - element.in_time[i])/10000}; work {element.work_time[i]/10000}");
-                Console.WriteLine("----------------------------------------------------------------------------------------------");
+                w += element.work_time[i];
+                //{element.state[i]} -> {element.state[i+1]}" + "|" 
+                summ += (element.out_time[i] - element.in_time[i]);
+            
+           //  Console.WriteLine($"Conv {i}:" + $"in q {element.in_time[i]}, out q {element.out_time[i]}, wait {(element.out_time[i] - element.in_time[i])}; work {element.work_time[i]}; workF {element.work_timeF[i]}");
+           //   Console.WriteLine("----------------------------------------------------------------------------------------------");
             }
+            ww += w;
             ss += summ;
-            Console.WriteLine($"All time wait {summ}");
-                Console.WriteLine("**************************************************************************************************");
+           // Console.WriteLine($"All time wait {summ}");
+           //     Console.WriteLine("**************************************************************************************************");
         }
     }
 }
